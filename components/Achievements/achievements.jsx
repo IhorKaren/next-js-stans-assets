f7231516f7a6facd5c1811b3d1e723db6ecdc5d3
Ihor Karen <karenihor@gmail.com>
2023-07-12 21:26:27 +0300

    achievements section

--- components/Achievements/achievements.jsx
+++ components/Achievements/achievements.jsx
@@ -1,15 +1,54 @@
 import Image from "next/image";
 
+import styles from "./achievements.module.scss";
+
+import achievements1 from "../../public/achievements/achievements1.png";
+import achievements2 from "../../public/achievements/achievements2.png";
+import achievements3 from "../../public/achievements/achievements3.png";
+import achievements4 from "../../public/achievements/achievements4.png";
+
 const Achievements = () => {
   return (
-    <section>
+    <section className={styles.container}>
       <div>
-        <h3>ACHIEVEMENTS</h3>
-        <ul>
-          <li><></></li>
-          <li></li>
-          <li></li>
-          <li></li>
+        <h3 className={styles.title}>ACHIEVEMENTS</h3>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <Image src={achievements1} alt="puzzles" className={styles.image} />
+            <p>
+              Collaboration with <strong>Moon Studios</strong> on WotW AAA Unity
+              game
+            </p>
+          </li>
+          <li className={styles.item}>
+            <Image src={achievements2} alt="rocket" className={styles.image} />
+            <p>
+              <span>50+</span> successfully launched projects on the market
+            </p>
+          </li>
+          <li className={styles.item}>
+            <Image
+              src={achievements3}
+              alt="phone"
+              width="204"
+              className={styles.image_centered}
+            />
+            <p>
+              Asset store publisher since <span>2013</span>
+            </p>
+          </li>
+          <li className={styles.item}>
+            <Image
+              src={achievements4}
+              alt="box"
+              width="210"
+              className={styles.image_centered}
+            />
+            <p>
+              Our products (packages) used by <span>100k+</span> devs in{" "}
+              <span>5k+</span> commercial projects worldwide
+            </p>
+          </li>
         </ul>
       </div>
     </section>

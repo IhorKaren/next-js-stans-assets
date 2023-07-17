4a4e5359174a765ce6854d55a43d88c762709321
Ihor Karen <karenihor@gmail.com>
2023-07-17 18:37:21 +0300

    Contacts page start

--- components/HeroBlock/heroBlock.jsx
+++ components/HeroBlock/heroBlock.jsx
@@ -2,21 +2,37 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./heroBlock.module.scss";
 
-const HeroBlock = ({ title, img, alt, route }) => {
+const HeroBlock = ({
+  title,
+  text,
+  img,
+  alt,
+  lastRoute = "Home",
+  routeName,
+  children,
+}) => {
+  const toLowerCaseRoute = lastRoute.split(" ").join("-").toLowerCase();
+
   return (
     <section className={styles.section}>
       <div className={styles.container}>
         <div>
           <span className={styles.navigate}>
-            <Link href="/">Home</Link> {">"} {route ? route : title}
+            <Link href={lastRoute ? `/${toLowerCaseRoute}` : "/"}>
+              {lastRoute}
+            </Link>
+            {" > "}
+            {routeName ? routeName : title}
           </span>
           <h1 className={styles.title}>{title}</h1>
+          {text && <p className={styles.text}>{text}</p>}
         </div>
         {img && (
           <div>
             <Image src={img} alt={alt} placeholder="blur" />
           </div>
         )}
+        {children}
       </div>
     </section>
   );
--- src/app/contacts/page.jsx
+++ src/app/contacts/page.jsx
@@ -1,7 +1,26 @@
-import Contacts from "../../../components/Contacts/contacts";
+import HeroBlock from "../../../components/HeroBlock/heroBlock";
+import Form from "../../../components/Form/form";
+import Looking from "../../../components/Looking/looking";
+import ProjectsSlider from "../../../components/ProjectsSlider/projectsSlider";
 
 const ContactsPage = () => {
-  return <Contacts />;
+  return (
+    <>
+      <HeroBlock
+        title="Contact Us"
+        lastRoute="About Us"
+        text="We are open for dialog and you are welcome to share your 
+      opinions or thoughts with us"
+      >
+        <Form
+          btnText="SEND REQUEST"
+          text="Hit us up any time through email with any idea and help us turn your great ideas into brilliant projects"
+        />
+      </HeroBlock>
+      <Looking />
+      <ProjectsSlider />
+    </>
+  );
 };
 
 export default ContactsPage;

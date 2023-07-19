9e3e9497f09d7ce2c1a486736907c5d33395fb0b
Ihor Karen <karenihor@gmail.com>
2023-07-19 11:55:17 +0300

    Lazy load?

--- components/Achievements/achievements.jsx
+++ components/Achievements/achievements.jsx
@@ -1,4 +1,11 @@
+import Image from "next/image";
+
 import styles from "./achievements.module.scss";
+
+import achievements1 from "../../public/achievements/achievements1.png";
+import achievements2 from "../../public/achievements/achievements2.png";
+import achievements3 from "../../public/achievements/achievements3.png";
+import achievements4 from "../../public/achievements/achievements4.png";
 
 const Achievements = () => {
   return (
@@ -7,28 +14,54 @@ const Achievements = () => {
         <h3 className={styles.title}>ACHIEVEMENTS</h3>
         <ul className={styles.list}>
           <li className={styles.item}>
-            <div className={styles.decoration}></div>
+            <Image
+              src={achievements1}
+              alt="puzzles"
+              className={styles.image}
+              placeholder="blur"
+              loading="lazy"
+            />
             <p>
               Collaboration with <strong>Moon Studios</strong> on WotW AAA Unity
               game
             </p>
           </li>
           <li className={styles.item}>
-            <div className={styles.decoration}></div>
+            <Image
+              src={achievements2}
+              alt="rocket"
+              className={styles.image}
+              placeholder="blur"
+              loading="lazy"
+            />
             <p className={styles.text}>
               <span className={styles.accent}>50+</span> successfully launched
               projects on the market
             </p>
           </li>
           <li className={styles.item}>
-            <div className={styles.decoration}></div>
+            <Image
+              src={achievements3}
+              alt="phone"
+              width="214"
+              className={styles.image_centered}
+              placeholder="blur"
+              loading="lazy"
+            />
             <p className={styles.text}>
               Asset store publisher since
               <span className={styles.accent}> 2013</span>
             </p>
           </li>
           <li className={styles.item}>
-            <div className={styles.decoration}></div>
+            <Image
+              src={achievements4}
+              alt="box"
+              width="260"
+              className={styles.image_centered}
+              placeholder="blur"
+              loading="lazy"
+            />
             <p className={styles.text}>
               Our products (packages) used by
               <span className={styles.accent}> 100k+</span> devs in

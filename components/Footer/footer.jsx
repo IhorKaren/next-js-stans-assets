b64e61399021e519c10c6644d5f2fc17375dd2b3
Ihor Karen <karenihor@gmail.com>
2023-07-24 15:31:13 +0300

    footer nav

--- components/Footer/footer.jsx
+++ components/Footer/footer.jsx
@@ -59,102 +59,107 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <ul className={styles.navigation}>
-            <li>
-              <h4 className={styles.title}>SERVICES</h4>
-              <ul className={styles.pages_list}>
-                <li>
-                  <Link
-                    href="/mobile-development"
-                    className={styles.navigation_link}
-                  >
-                    Mobile Application Development
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/game-development"
-                    className={styles.navigation_link}
-                  >
-                    Game Development Outsourcing
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/vr-development"
-                    className={styles.navigation_link}
-                  >
-                    VR App Development
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/sdk-development"
-                    className={styles.navigation_link}
-                  >
-                    SDK Development
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/3d-modeling" className={styles.navigation_link}>
-                    3D Modeling services
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <h4 className={styles.title}>TECHNOLOGIES</h4>
-              <ul className={styles.pages_list}>
-                <li>
-                  <Link href="/android" className={styles.navigation_link}>
-                    Android
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/ios" className={styles.navigation_link}>
-                    IOS
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/unity" className={styles.navigation_link}>
-                    Unity
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <ul className={styles.main_pages_list}>
-                <li>
-                  <Link href="/products">PRODUCTS</Link>
-                </li>
-                <li>
-                  <Link href="/portfolio">PORTFOLIO</Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link href="/about-us" className={styles.title}>
-                ABOUT US
-              </Link>
-              <ul className={styles.pages_list}>
-                <li>
-                  <Link href="/contacts" className={styles.navigation_link}>
-                    Contacts
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/team" className={styles.navigation_link}>
-                    Team
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/join" className={styles.navigation_link}>
-                    Join the Team
-                  </Link>
-                </li>
-              </ul>
-            </li>
-          </ul>
+          <nav className={styles.navigation}>
+            <ul className={styles.navigation_list}>
+              <li>
+                <h4 className={styles.title}>SERVICES</h4>
+                <ul className={styles.pages_list}>
+                  <li>
+                    <Link
+                      href="/mobile-development"
+                      className={styles.navigation_link}
+                    >
+                      Mobile Application Development
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="/game-development"
+                      className={styles.navigation_link}
+                    >
+                      Game Development Outsourcing
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="/vr-development"
+                      className={styles.navigation_link}
+                    >
+                      VR App Development
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="/sdk-development"
+                      className={styles.navigation_link}
+                    >
+                      SDK Development
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="/3d-modeling"
+                      className={styles.navigation_link}
+                    >
+                      3D Modeling services
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <h4 className={styles.title}>TECHNOLOGIES</h4>
+                <ul className={styles.pages_list}>
+                  <li>
+                    <Link href="/android" className={styles.navigation_link}>
+                      Android
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/ios" className={styles.navigation_link}>
+                      IOS
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/unity" className={styles.navigation_link}>
+                      Unity
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <ul className={styles.main_pages_list}>
+                  <li>
+                    <Link href="/products">PRODUCTS</Link>
+                  </li>
+                  <li>
+                    <Link href="/portfolio">PORTFOLIO</Link>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <Link href="/about-us" className={styles.title}>
+                  ABOUT US
+                </Link>
+                <ul className={styles.pages_list}>
+                  <li>
+                    <Link href="/contacts" className={styles.navigation_link}>
+                      Contacts
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/team" className={styles.navigation_link}>
+                      Team
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/join" className={styles.navigation_link}>
+                      Join the Team
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
         </div>
       </footer>
       <div className={styles.wrap}>
